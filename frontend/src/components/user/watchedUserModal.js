2c758cc7130a602d83fc61252eb6c961f2aa92b3
hishira <okami0124@gmail.com>
2020-09-17 13:21:19 +0200

    Change watchedusermodal component arrangement

--- frontend/src/components/user/watchedUserModal.js
+++ frontend/src/components/user/watchedUserModal.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Modal,Container,List } from "semantic-ui-react";
+import { Modal,Container,List,Button } from "semantic-ui-react";
 import { inject, observer } from "mobx-react";
 import { getWatchedUser } from "../../api/userApi";
 import Loading from "../shared/loadingComponent";
+import { useHistory } from "react-router-dom";
+
 function WatchedUsersModal({ user, mainStore }) {
   const [watchedUsers, setWatchedUsers] = useState({});
   const [loading, setLoading] = useState("false");
+  const history = useHistory();
   useEffect(() => {
     let obj = { userID: user._id };
     const fetchData = async () => {
@@ -38,11 +41,33 @@ function WatchedUsersModal({ user, mainStore }) {
         <div>Error</div>
       ) : (
         <List divided relaxed>
-            {watchedUsers.map(watch=><List.Item>
-                <List.Content>
-                    {watch.email}
-                </List.Content>
-            </List.Item>)}
+             {watchedUsers.map((user) => (
+            <List.Item
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "1rem",
+                alignItems: "center",
+              }}
+            >
+              <List.Icon name="user" size="large" verticalAlign="middle" />
+              <List.Content
+                style={{
+                  display: "flex",
+                  width: "25rem",
+                  justifyContent: "space-between",
+                  alignItems:"center",
+                  marginLeft: "2rem",
+                }}
+              >
+                <List.Header as="h4">{user.username}</List.Header>
+                <Button icon="user" onClick={()=>{
+                  history.push(`/userprofile/${user._id}`);
+                  mainStore.setWatchedUserModal(false)
+                  }} />
+              </List.Content>
+            </List.Item>
+          ))}
             
         </List>
       )}
