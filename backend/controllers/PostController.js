ae8f428db24c14a5546a23f2f142b394f92d7f29
hishira <okami0124@gmail.com>
2020-10-21 20:24:12 +0200

    Create post controller

---
+++ backend/controllers/PostController.js
@@ -0,0 +1,252 @@
+const postModel = require("../models/Post");
+const userModel = require("../models/User");
+const commentModel = require("../models/Comment");
+
+class PostController {
+  static async GetAllPosts(req, res) {
+    try {
+      const posts = await postModel.find({});
+      try {
+        res.send(posts);
+      } catch (err) {
+        res.status(404).send(err);
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+  static async GetPublicPosts(req, res) {
+    try {
+      const publicPosts = await postModel.find({ postType: "PUBLIC" });
+      try {
+        res.status(200).send(publicPosts);
+      } catch (err) {
+        res.status(404).send(err);
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+  static async GetPrivatePosts(req, res) {
+    try {
+      const privateposts = await postModel.find({ postType: "PRIVATE" });
+      try {
+        res.status(200).send(privateposts).end();
+      } catch (err) {
+        res.status(404).send(err).end();
+      }
+    } catch (err) {
+      res.status(500).send(err).end();
+    }
+  }
+  static async GetUserPosts(req, res) {
+    try {
+      let userPosts = await postModel.find({ user: req.user._id });
+      try {
+        res.status(200).send(userPosts).end();
+      } catch (err) {
+        res.status(404).json(err).end();
+      }
+    } catch (err) {
+      res.status(500).send(err).end();
+    }
+  }
+  static async GetPublicUserPosts(req, res) {
+    try {
+      let publicUserPosts = await postModel.findById(req.params.id);
+      try {
+        const postwithcomments = await publicUserPosts
+          .populate("comments")
+          .execPopulate();
+        res
+          .status(200)
+          .json({ post: postwithcomments, comments: postwithcomments });
+      } catch (err) {
+        res.status(404).json(err).end();
+      }
+    } catch (err) {
+      res.status(404).json(err).end();
+    }
+  }
+  static async Create(req, res) {
+    try {
+      console.log(req.body);
+      const newPost = new postModel({
+        title: req.body.title,
+        content: req.body.content,
+        user: req.user._id,
+        postType: req.body.postPrivate ? "PRIVATE" : "PUBLIC",
+        tags: req.body.tags,
+      });
+      try {
+        await newPost.save();
+        const user = await userModel.findById(req.user._id);
+        user.posts.push(newPost);
+        await user.save();
+        res.status(200).json(newPost);
+      } catch (err) {
+        res.status(500).send("Error with post adding");
+      }
+    } catch (err) {
+      res.status(500).send("error");
+    }
+  }
+  static async Update(req, res) {
+    try {
+      postModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          title: req.body.title,
+          content: req.body.content,
+          editingDate: Date.now(),
+        },
+        { new: true },
+        (err, model) => {
+          if (err) {
+            return res
+              .status(404)
+              .json({ message: "Erorro with post updating" })
+              .end();
+          }
+          if (model === null)
+            return res
+              .status(404)
+              .json({ message: "Erorro with post updating" })
+              .end();
+          else {
+            return res.status(200).json(model).end();
+          }
+        }
+      );
+    } catch (err) {
+      res.status(500).json({ message: err }).end();
+    }
+  }
+  static async MakePrivate(req, res) {
+    try {
+      await postModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          editingDate: Date.now(),
+          postType: "PRIVATE",
+        },
+        { new: true },
+        (err, model) => {
+          if (!err) {
+            res.status(200).json(model).end();
+          } else {
+            return res.status(404).send({ error: err }).end();
+          }
+        }
+      );
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  }
+  static async MakePublic(req, res) {
+    try {
+      await postModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          editingDate: Date.now(),
+          postType: "PUBLIC",
+        },
+        { new: true },
+        (err, model) => {
+          if (err) {
+            res.status(404).json({ message: "Error with editing post type" });
+          } else {
+            res.status(200).json(model);
+          }
+        }
+      );
+    } catch (err) {
+      res.status(500).json({ message: err });
+    }
+  }
+  static async Delete(req, res) {
+    try {
+      const post = await postModel.findByIdAndDelete(req.params.id);
+      if (!post) {
+        return res.status(404).send("Not post found");
+      }
+      const user = await userModel.findById(req.user._id);
+      user.posts.remove(req.params.id);
+      for (let i in post.comments) {
+        await commentModel.findByIdAndDelete(post.comments[i]);
+        await user.comments.remove(post.comments[i]);
+      }
+      await user.save();
+      res.status(200).send("Ok");
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+  static async Like(req,res){
+    try{
+        const post = await postModel.findById(req.params.id)
+        if (!post){
+          return res.status(404).send("Post not found")
+        }else{
+          const user = await userModel.findById(req.user._id)
+          if(post.likes.includes(user._id))
+            return res.status(400).send("User like this post")
+          post.likes.push(user)
+          await post.save()
+          return res.status(200).send("Ok")
+        }
+      }catch (err) {
+        return res.status(500).send("Server error")
+      }
+  }
+  static async RemoveLike(req,res){
+    try{
+        const post = await postModel.findById(req.params.id)
+        if (!post){
+          return res.status(404).send("Post not found")
+        }else{
+          const user = await userModel.findById(req.user._id)
+          if(!post.likes.includes(user._id))
+            return res.status(400).send("User do not like this post")
+          await post.likes.remove(user)
+          await post.save()
+          res.status(200).send("Ok")
+        }
+      }catch (err) {
+        res.status(500).send("Server error")
+      }
+  }
+  static async Sort(req,res){
+    try{
+        const post = await postModel.find({user:req.body.userID}).sort({createDate:(req.body.sortOption === "date_descending")?'descending':'ascending'})
+        return res.status(200).json(post).end()
+      }catch (err) {
+        return res.status(500).send("Server problem")
+      }
+  }
+  static async UserPosts(req,res){
+    try{
+        const posts = await postModel.find({user:req.params.id})
+        return res.status(200).json(posts)
+      }catch(err){
+        return res.status(505).send("Server error")
+      }
+  }
+  static async SortByLikes(req,res){
+    try{
+        const posts = await postModel.find({user:req.body.userID}).sort({likes:-1})
+        return res.status(200).json(posts)
+      }catch(err){
+        return res.status(500).send("server error")
+      }
+  }
+  static async PostsByTag(req,res){
+    try{
+        const posts = await postModel.find({tags:{$all:[req.body.username]}})
+        return res.status(200).json(posts)
+    }catch(err){
+      return res.status(500).send("server error")
+    }
+  }
+}
+module.exports = PostController
--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,267 +1,47 @@
 const express = require("express");
-const postModel = require("../models/Post");
-const userModel = require("../models/User");
-const commentModel = require("../models/Comment")
+const PostController = require('../controllers/PostController');
 const app = express();
 const checkifAdmin = require("../middleware/ifadmin");
 const checkifLogin = require("../middleware/logedin");
 
-app.get("/posts", checkifAdmin, async (req, res) => {
-  try {
-    const posts = await postModel.find({});
-    try {
-      res.send(posts);
-    } catch (err) {
-      res.status(404).send(err);
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+app.get("/posts", checkifAdmin, PostController.GetAllPosts);
 /*
   Return only public posts
 */
-app.get("/publicposts", checkifLogin, async (req, res) => {
-  try {
-    const publicPosts = await postModel.find({ postType: "PUBLIC" });
-    try {
-      res.status(200).send(publicPosts);
-    } catch (err) {
-      res.status(404).send(err);
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+app.get("/publicposts", checkifLogin,PostController.GetPublicPosts);
 /*
   Return all private post, for admin only
 */
-app.get("/privateposts", checkifAdmin, async (req, res) => {
-  try {
-    const privateposts = await postModel.find({ postType: "PRIVATE" });
-    try {
-      res.status(200).send(privateposts).end();
-    } catch (err) {
-      res.status(404).send(err).end();
-    }
-  } catch (err) {
-    res.status(500).send(err).end();
-  }
-});
+app.get("/privateposts", checkifAdmin,PostController.GetPrivatePosts);
 /* 
   Return all, private and public user posts
 */
-app.get("/userposts", checkifLogin, async (req, res) => {
-  try {
-    let userPosts = await postModel.find({ user: req.user._id });
-    try {
-      res.status(200).send(userPosts).end();
-    } catch (err) {
-      res.status(404).json(err).end();
-    }
-  } catch (err) {
-    res.status(500).send(err).end();
-  }
-});
+app.get("/userposts", checkifLogin, PostController.GetUserPosts);
 /*
   Return public post by id
 */
-app.get("/publicuserpost/:id", async (req, res) => {
-  try {
-    let publicUserPosts = await postModel.findById(req.params.id)
-    try{
-        const postwithcomments = await publicUserPosts.populate('comments').execPopulate()
-        res.status(200).json({post:postwithcomments,comments:postwithcomments})
-    }catch (err) {
-        res.status(404).json(err).end();
-      }
-    }catch(err){
-        res.status(404).json(err).end()
-    }
-});
-app.post("/post", checkifLogin, async (req, res) => {
-  try {
-    console.log(req.body)
-    const newPost = new postModel({
-      title: req.body.title,
-      content: req.body.content,
-      user: req.user._id,
-      postType: (req.body.postPrivate)? "PRIVATE":"PUBLIC",
-      tags:req.body.tags
-    });
-    try {
-      await newPost.save();
-      const user = await userModel.findById(req.user._id);
-      user.posts.push(newPost);
-      await user.save();
-      res.status(200).json(newPost);
-    } catch (err) {
-      res.status(500).send("Error with post adding");
-    }
-  } catch (err) {
-    res.status(500).send("error");
-  }
-});
+app.get("/publicuserpost/:id", PostController.GetPublicUserPosts);
+app.post("/post", checkifLogin, PostController.Create);
 
 /*
   Edit post by id
 */
-app.put("/post/:id",checkifLogin, async (req, res) => {
-  try {
-    postModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-        content: req.body.content,
-        editingDate: Date.now(),
-      },
-      {new:true},
-      (err, model) => {
-        if (err) {
-          return res.status(404).json({ message: "Erorro with post updating" }).end();
-        } 
-        if(model === null)
-          return res.status(404).json({ message: "Erorro with post updating" }).end();
-        else {
-          return res.status(200).json(model).end();
-        }
-      }
-    );
-  } catch (err) {
-    res.status(500).json({ message: err }).end();
-  }
-});
+app.put("/post/:id",checkifLogin, PostController.Update);
 
 /* 
     Function that make post private
 */
-app.put("/postprivate/:id", checkifLogin,async (req, res) => {
-  try {
-    await postModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        editingDate: Date.now(),
-        postType: "PRIVATE",
-      },{new:true},
-      (err, model) => {
-        if (!err) {
-          res.status(200).json(model).end();
-        } else {
-          return res.status(404).send({error: err}).end();
-        }
-      }
-    );
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+app.put("/postprivate/:id", checkifLogin,PostController.MakePrivate);
 
 /* 
     Function that make post public
 */
-app.put("/publicpost/:id",checkifLogin, async (req, res) => {
-  
-  try {
-    await postModel.findByIdAndUpdate(
-      req.params.id,
-      {
-        editingDate: Date.now(),
-        postType: "PUBLIC",
-      },{new:true},
-      (err, model) => {
-        if (err) {
-          res.status(404).json({ message: "Error with editing post type" });
-        } else {
-          res.status(200).json(model);
-        }
-      }
-    );
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
-app.delete("/post/:id", checkifLogin, async (req, res) => {
-  try {
-    const post = await postModel.findByIdAndDelete(req.params.id);
-    if (!post) {
-      return res.status(404).send("Not post found");
-    }
-    const user = await userModel.findById(req.user._id);
-    user.posts.remove(req.params.id);
-    for(let i in post.comments){
-      await commentModel.findByIdAndDelete(post.comments[i])
-      await user.comments.remove(post.comments[i])
-    }
-    await user.save()
-    res.status(200).send("Ok");
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-app.post("/post/likeadd/:id",checkifLogin,async(req,res)=>{
-  try{
-    const post = await postModel.findById(req.params.id)
-    if (!post){
-      return res.status(404).send("Post not found")
-    }else{
-      const user = await userModel.findById(req.user._id)
-      if(post.likes.includes(user._id))
-        return res.status(400).send("User like this post")
-      post.likes.push(user)
-      await post.save()
-      return res.status(200).send("Ok")
-    }
-  }catch (err) {
-    return res.status(500).send("Server error")
-  }
-})
-app.post("/post/likeremove/:id",checkifLogin,async(req,res)=>{
-  try{
-    const post = await postModel.findById(req.params.id)
-    if (!post){
-      return res.status(404).send("Post not found")
-    }else{
-      const user = await userModel.findById(req.user._id)
-      if(!post.likes.includes(user._id))
-        return res.status(400).send("User do not like this post")
-      await post.likes.remove(user)
-      await post.save()
-      res.status(200).send("Ok")
-    }
-  }catch (err) {
-    res.status(500).send("Server error")
-  }
-})
-app.post("/sortpost",async (req,res)=>{
-  try{
-    const post = await postModel.find({user:req.body.userID}).sort({createDate:(req.body.sortOption === "date_descending")?'descending':'ascending'})
-    return res.status(200).json(post).end()
-  }catch (err) {
-    return res.status(500).send("Server problem")
-  }
-})
-app.get("/userposts/:id",checkifAdmin,async(req,res)=>{
-  try{
-    const posts = await postModel.find({user:req.params.id})
-    return res.status(200).json(posts)
-  }catch(err){
-    return res.status(505).send("Server error")
-  }
-})
-app.post("/sortpostsbylikes",async(req,res)=>{
-  try{
-    const posts = await postModel.find({user:req.body.userID}).sort({likes:-1})
-    return res.status(200).json(posts)
-  }catch(err){
-    return res.status(500).send("server error")
-  }
-})
-app.post("/getpostsbytag",async(req,res)=>{
-  try{
-      const posts = await postModel.find({tags:{$all:[req.body.username]}})
-      return res.status(200).json(posts)
-  }catch(err){
-    return res.status(500).send("server error")
-  }
-})
+app.put("/publicpost/:id",checkifLogin, PostController.MakePublic);
+app.delete("/post/:id", checkifLogin,PostController.Delete);
+app.post("/post/likeadd/:id",checkifLogin,PostController.Like);
+app.post("/post/likeremove/:id",checkifLogin,PostController.RemoveLike);
+app.post("/sortpost",PostController.Sort);
+app.get("/userposts/:id",checkifAdmin,PostController.UserPosts);
+app.post("/sortpostsbylikes",PostController.SortByLikes);
+app.post("/getpostsbytag",PostController.PostsByTag);
 module.exports = app;
